70bfb83b1f5e0256c502713ca843960d814f176b
Alexandre Russi <junior.russi2011@gmail.com>
2020-06-10 23:44:30 -0300

    output data and naive bayes fixed

--- naive_bayes_clf.py
+++ naive_bayes_clf.py
@@ -4,12 +4,13 @@ import ia_n2020
 import pandas as pd
 
 input_values, contaminated = ia_n2020.ioValues()
+predict_values = ia_n2020.newData()
 input_values_2d, predict_values_2d = pca_graph.normalize_data_2d()
 
 nb_clf = GaussianNB()
-nb_clf.fit(input_values_2d, contaminated)
-nb_score = nb_clf.score(input_values_2d, contaminated)
-nb_predict = nb_clf.predict(predict_values_2d)
+nb_clf.fit(input_values, contaminated)
+nb_score = nb_clf.score(input_values, contaminated)
+nb_predict = nb_clf.predict(predict_values)
 
 print(nb_score)
 print(nb_predict)
@@ -18,4 +19,8 @@ spreadsheet = pd.read_csv('output_data/predict_data.csv')
 spreadsheet['nb_predict'] = nb_predict
 spreadsheet.to_csv('output_data/predict_data.csv', index=False)
 
-ia_n2020.plotGraph2D(input_values_2d, contaminated, nb_clf, 'naive_bayes_2d')
+nb_clf_2d = GaussianNB()
+nb_clf_2d.fit(input_values_2d, contaminated)
+nb_predict_2d = nb_clf_2d.predict(predict_values_2d)
+
+ia_n2020.plotGraph2D(input_values_2d, contaminated, nb_clf_2d, 'naive_bayes_2d')
